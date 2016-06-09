ff8f1497cf02d5e7478843e8d0adaa6d893e5687
Brahaj, Armand <Armand.Brahaj@fiz-Karlsruhe.de>
2016-06-09 11:46:40 +0200

    Comment Tinymce

--- grails-app/assets/javascripts/tinymceboot.js
+++ grails-app/assets/javascripts/tinymceboot.js
@@ -1,7 +1,11 @@
+/**
+ * @Author arb
+ * Boot TinyMce for use
+ */
 if (typeof jQuery !== 'undefined') {
     (function($) {
+        //use window.bind to load the tinymce per last. Otherwise will throw error
         $(window).bind("load", function() {
-
         tinymce.init({
             selector: 'textarea',
             plugin: 'a_tinymce_plugin',
